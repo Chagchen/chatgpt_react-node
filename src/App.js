d0b6b5fb4e7ea849a3e62a63f3fed5def0babbc7
chagchen <chagchen@outlook.com>
2023-05-25 14:03:00 -0700

    added front end styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './index.css'
+import './index.css';
 
 
 const App = () => {
@@ -6,14 +6,29 @@ const App = () => {
     <div className="app">
       <section className="side-bar">
         <button >+ New Chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
           <li>Test</li>
+        </ul>
         <nav>
           <p>Made by Bo</p>
         </nav>
       </section>
-
+      
       <section className="main">
+        <h2>Bo's GPT</h2>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit">➣</div>
+          </div>
+          <p className="info">
+            Chat GPT 4 version. Free Research preview. Out goal is to make AI systems more natural and safe
+            to interact with. Yout feeback will help us improve.
+          </p>
+        </div>
       </section>
     </div>
   );
